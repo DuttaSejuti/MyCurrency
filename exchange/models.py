136c925324b68d566f74c35ac4fa4bd9f8030af2
DuttaSejuti <duttasejuti24@gmail.com>
2026-03-20 20:53:44 +0100

    edit currencys to currencies for admin view

--- exchange/models.py
+++ exchange/models.py
@@ -4,6 +4,9 @@ class Currency(models.Model):
     # e.g. EUR, USD, CHF, GBP
     code = models.CharField(max_length=3, unique=True)
     name = models.CharField(max_length=50, blank=True)
+
+    class Meta:
+        verbose_name_plural = 'Currencies'
         
     def __str__(self):
         return self.code
