48fb80f06761d35e392577ff0bc3e1951a26fabb
DuttaSejuti <duttasejuti24@gmail.com>
2026-03-22 02:09:21 +0100

    create currency CRUD

--- exchange/views.py
+++ exchange/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Currency
+from .serializers import CurrencySerializer
 
-# Create your views here.
+class CurrencyViewSet(viewsets.ModelViewSet):
+    queryset = Currency.objects.all()
+    serializer_class = CurrencySerializer
